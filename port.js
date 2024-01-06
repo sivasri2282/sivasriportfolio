8e6039fe32e397d3bdbfd6a6a0190b02bd06765a
sivasri2282 <155841252+sivasri2282@users.noreply.github.com>
2024-01-06 23:10:06 +0530

    Add files via upload

---
+++ port.js
@@ -0,0 +1,22 @@
+function home() {
+    var targetSection = document.querySelector('#home');
+    targetSection.scrollIntoView({ behavior: 'smooth' });
+  }
+  
+  
+  
+  function about() {
+      var targetSection = document.querySelector('#about');
+      targetSection.scrollIntoView({ behavior: 'smooth' });
+    }
+    
+  
+    function skills() {
+      var targetSection = document.querySelector('#skills');
+      targetSection.scrollIntoView({ behavior: 'smooth' });
+    }
+    
+    function contanct() {
+      var targetSection = document.querySelector('#contanct');
+      targetSection.scrollIntoView({ behavior: 'smooth' });
+    }
